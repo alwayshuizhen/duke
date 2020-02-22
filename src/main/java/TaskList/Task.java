62cd36051c44fe47786fd859ed1a9cf2bca2004b
alwayshuizhen <32288711+alwayshuizhen@users.noreply.github.com>
2020-02-22 20:21:18 +0800

    Revert "Find"

--- src/main/java/TaskList/Task.java
+++ src/main/java/TaskList/Task.java
@@ -12,8 +12,6 @@ public class Task {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    public String getDescription() {return description;}
-
     public void setDone() {
         this.isDone = true;
     }
